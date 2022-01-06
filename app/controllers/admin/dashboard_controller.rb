3e9442f897edfe0ba57fa2ba0a4b71cb347726fe
Walid <walid_douri@hotmail.com>
2022-01-06 17:04:03 -0500

    Added Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => "Jungle", :password => "book"
 
   def show
+    @category_list = Category.all
     
+    @product_count = Product.count();
+
   end
 end
